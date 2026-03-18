6c592386aaca1fc624265344e338a98991ed8c01
KUMA-Ani <kuma.animation2026@gmail.com>
2026-03-19 01:08:55 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // 사이트의 기본 경로를 저장소 이름과 똑같이 맞춰줍니다.
-  base: '/2026-KUMA-Ani', 
-  // 실제 배포될 도메인 주소 (선택사항이지만 권장)
-  site: 'https://2026-kuma-ani.vercel.app',
+  base: '/', // 주소창에서 리포지토리 이름을 제거합니다.
 });
